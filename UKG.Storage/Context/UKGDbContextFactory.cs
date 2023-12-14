e934d84a3940316b0f8b7298486cf357d35aca02
paczulapiotr <paczulapiotr@gmail.com>
2023-12-14 22:40:00 +0100

    Add basic sql migration

---
+++ UKG.Storage/Context/UKGDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace UKG.Storage.Context;
+
+public class UKGDbContextFactory : IDesignTimeDbContextFactory<UKGDbContext>
+{
+    public UKGDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<UKGDbContext>();
+        optionsBuilder.UseSqlite("Data Source=ukg_database.db");
+
+        return new UKGDbContext(optionsBuilder.Options);
+    }
+}
