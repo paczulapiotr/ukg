5e295535d9ed058491d56504f58da36c3dad4ce4
paczulapiotr <paczulapiotr@gmail.com>
2023-12-14 17:51:59 +0100

    Add test projects

---
+++ UKG.Storage.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace UKG.Storage.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
