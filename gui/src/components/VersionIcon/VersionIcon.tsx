ae618e0c043f7a9268b68d801c572be013b49654
paczulapiotr <paczulapiotr@gmail.com>
2024-07-17 14:53:10 +0200

    remove ukg-frontend directory

--- gui/src/components/VersionIcon/VersionIcon.tsx
+++ gui/src/components/VersionIcon/VersionIcon.tsx
@@ -2,6 +2,7 @@ import { Tooltip } from "antd";
 import { InfoCircleOutlined } from "@ant-design/icons";
 import useGetApiStatus from "@/queries/useGetStatus";
 import packageJson from "../../../package.json";
+import { GithubOutlined } from "@ant-design/icons";
 
 const VersionIcon = () => {
   const { data } = useGetApiStatus();
@@ -13,6 +14,13 @@ const VersionIcon = () => {
           <span>{`Wersja API: ${data?.version ?? "N/A"}`}</span>
           <br />
           <span>{`Wersja GUI: ${packageJson.version}`}</span>
+          <br />
+          <span>
+            {"Kod źródłowy: "}
+            <a href="https://github.com/paczulapiotr/ukg" target="_blank">
+              <GithubOutlined />
+            </a>
+          </span>
         </>
       }
     >
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,11 @@
 # Get the absolute path of the project root directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-# Remove all files in gui/ukg-frontend/src-tauri/server
-rm -rf "$SCRIPT_DIR/gui/ukg-frontend/src-tauri/server"/*
+# Remove all files in gui/src-tauri/server
+rm -rf "$SCRIPT_DIR/gui/src-tauri/server"/*
 
-# Publish UKG.Api/UKG.Api.csproj in debug mode to gui/ukg-frontend/src-tauri/server
-dotnet publish "$SCRIPT_DIR/UKG.Api/UKG.Api.csproj" -c Debug -o "$SCRIPT_DIR/gui/ukg-frontend/src-tauri/server"
+# Publish UKG.Api/UKG.Api.csproj in debug mode to gui/src-tauri/server
+dotnet publish "$SCRIPT_DIR/UKG.Api/UKG.Api.csproj" -c Debug -o "$SCRIPT_DIR/gui/src-tauri/server"
 
 # Change directory to gui/ukg-frontend using pushd
 pushd "$SCRIPT_DIR/gui/ukg-frontend"
